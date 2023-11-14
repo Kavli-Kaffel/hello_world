a8b452dcdc3a719ef927f1ea1de2a021753aff52
Kavli-Kaffel <imakii245@outlook.com>
2023-11-14 22:01:13 +0300

    Squashed commit of the following:
    commit 846111d1afa6fd4410e17a0250e12fe483a7d00d
    Author: Kavli-Kaffel <imakii245@outlook.com>
    Date:   Tue Nov 14 21:54:02 2023 +0300
    
        Научились читать имя
    
    commit a28b4e7c1d0ebcd118373619eeba801af74e7716
    Author: Kavli-Kaffel <imakii245@outlook.com>
    Date:   Tue Nov 14 21:38:40 2023 +0300
    
        Начали работать над приветствием

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,14 @@
+def greeting(name: str):
+    print(f"Hello {name}!")
+
+
+def whats_your_name() -> str:
+    return input("What's your name?\n")
+
+
 def main():
-    print("Hello world!!!")
+    name = whats_your_name()
+    greeting(name)
 
 
 if __name__ == "__main__":
